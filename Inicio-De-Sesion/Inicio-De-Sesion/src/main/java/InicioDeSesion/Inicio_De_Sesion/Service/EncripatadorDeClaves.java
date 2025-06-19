dd960de23f2b4651d836b51877d8a8889adea2c7
xiomararomero98 <xio.romero@duocuc.cl>
2025-06-19 14:29:49 -0400

    q

--- Inicio-De-Sesion/Inicio-De-Sesion/src/main/java/InicioDeSesion/Inicio_De_Sesion/Service/EncripatadorDeClaves.java
+++ Inicio-De-Sesion/Inicio-De-Sesion/src/main/java/InicioDeSesion/Inicio_De_Sesion/Service/EncripatadorDeClaves.java
@@ -1,5 +1,14 @@
 package InicioDeSesion.Inicio_De_Sesion.Service;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class EncripatadorDeClaves {
+    public static String encriptar(String clave) {
+        return BCrypt.hashpw(clave, BCrypt.gensalt());
+    }
 
+    public static boolean comparar(String clave, String hashGuardado) {
+        return BCrypt.checkpw(clave, hashGuardado);
+    }
+
+    
 }
